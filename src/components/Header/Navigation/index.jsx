c7def6dbea7e13fbf44e90e4ae5fc690abf0e82e
Thanh Nguyen <thnhnguyn761@gmail.com>
2025-03-23 21:33:58 +0700

    Update header

--- src/components/Header/Navigation/index.jsx
+++ src/components/Header/Navigation/index.jsx
@@ -2,11 +2,12 @@ import { Button } from '@mui/material';
 import React from 'react'
 import { RiMenu2Fill } from "react-icons/ri";
 import { LiaAngleDownSolid } from "react-icons/lia";
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
     return (
-        <nav>
-            <div className='container flex items-center justify-end'>
+        <nav className='py-2'>
+            <div className='container flex items-center justify-end gap-8'>
                 <div className='col_1 w-[20%]'>
                     <Button className='!text-black gap-2 w-full'>
                         <RiMenu2Fill className='text-[18px]' />
@@ -15,7 +16,26 @@ const Navigation = () => {
                     </Button>
                 </div>
                 <div className='col_2 w-[80%]'>
-                    <ul className='flex items-center'></ul>
+                    <ul className='flex items-center gap-3'>
+                        <li className='list-none'>
+                            <Link to='/' className='link transition text-[14px] font-[500]'>Home</Link>
+                        </li>
+                        <li className='list-none'>
+                            <Link to='/' className='link transition text-[14px] font-[500]'>Home</Link>
+                        </li>
+                        <li className='list-none'>
+                            <Link to='/' className='link transition text-[14px] font-[500]'>Home</Link>
+                        </li>
+                        <li className='list-none'>
+                            <Link to='/' className='link transition text-[14px] font-[500]'>Home</Link>
+                        </li>
+                        <li className='list-none'>
+                            <Link to='/' className='link transition text-[14px] font-[500]'>Home</Link>
+                        </li>
+                        <li className='list-none'>
+                            <Link to='/' className='link transition text-[14px] font-[500]'>Home</Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
