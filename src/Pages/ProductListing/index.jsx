b81bef21ed55247ca0134ac19d4bc223e0573150
Thanh Nguyen <thnhnguyn761@gmail.com>
2025-03-29 04:47:28 +0700

    Update PL Page

--- src/Pages/ProductListing/index.jsx
+++ src/Pages/ProductListing/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import Sidebar from '../../components/Sidebar';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
 
 const ProductListing = () => {
     return (
         <section className='py-8'>
+            <div className='container'>
+                <Breadcrumbs aria-label="breadcrumb">
+                    <Link underline="hover" color="inherit" href="/">
+                        Home
+                    </Link>
+                    <Link
+                        underline="hover"
+                        color="inherit"
+                        href="/"
+                    >
+                        Fashion
+                    </Link>
+                    <Typography sx={{ color: 'text.primary' }}>Breadcrumbs</Typography>
+                </Breadcrumbs>
+            </div>
             <div className='container flex gap-3'>
-                <div className='sidebarWrapper'>
+                <div className='sidebarWrapper w-[20%] h-full bg-white p-3'>
                     <Sidebar />
                 </div>
             </div>
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <aside className='sidebar'>
+            <div className='box'>
+                <h3>Shop by Category</h3>
+            </div>
+        </aside>
     )
 }
 export default Sidebar;
