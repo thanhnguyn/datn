48e933f090bdb183c6b5fe87a4a1dfd47400f455
Thanh Nguyen <thnhnguyn761@gmail.com>
2025-03-30 03:50:53 +0700

    Update PL Page

--- src/Pages/ProductListing/index.jsx
+++ src/Pages/ProductListing/index.jsx
@@ -3,6 +3,7 @@ import Sidebar from '../../components/Sidebar';
 import Typography from '@mui/material/Typography';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
+import ProductItem from '../../components/ProductItem';
 
 const ProductListing = () => {
     return (
@@ -26,6 +27,20 @@ const ProductListing = () => {
                 <div className='container flex gap-3'>
                     <div className='sidebarWrapper w-[20%] h-full bg-white'>
                         <Sidebar />
+                    </div>
+
+                    <div className='rightContent w-[80%]'>
+                        <div className='grid grid-cols-4 md:grid-cols-4 gap-4'>
+                            <ProductItem />
+                            <ProductItem />
+                            <ProductItem />
+                            <ProductItem />
+                            <ProductItem />
+                            <ProductItem />
+                            <ProductItem />
+                            <ProductItem />
+
+                        </div>
                     </div>
                 </div>
             </div>
