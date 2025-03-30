cabbe88d436c48d9f355d2dada5af3912f4c01dc
Thanh Nguyen <thnhnguyn761@gmail.com>
2025-03-31 06:06:22 +0700

    Update PD page

--- src/Pages/ProductDetails/index.jsx
+++ src/Pages/ProductDetails/index.jsx
@@ -2,32 +2,68 @@ import { Breadcrumbs } from '@mui/material';
 import React from 'react'
 import { Link } from 'react-router-dom';
 import ProductZoom from '../../components/ProductZoom';
+import Rating from '@mui/material/Rating';
 
 const ProductDetails = () => {
     return (
-        <section className='py-5 pb-0'>
-            <div className='container'>
-                <Breadcrumbs aria-label="breadcrumb">
-                    <Link underline="hover" color="inherit" href="/" className='link transition'>
-                        Home
-                    </Link>
-                    <Link
-                        underline="hover"
-                        color="inherit"
-                        href="/"
-                        className='link'
-                    >
-                        Fashion
-                    </Link>
-                </Breadcrumbs>
-            </div>
-
-            <div className='container flex gap-4'>
-                <div className='productZoomContainer'>
-                    <ProductZoom />
+        <>
+            <div className='py-5'>
+                <div className='container'>
+                    <Breadcrumbs aria-label="breadcrumb">
+                        <Link
+                            underline="hover"
+                            color="inherit"
+                            href="/"
+                            className='link transition !text-[14px]'
+                        >
+                            Home
+                        </Link>
+                        <Link
+                            underline="hover"
+                            color="inherit"
+                            href="/"
+                            className='link transition !text-[14px]'
+                        >
+                            Fashion
+                        </Link>
+                        <Link
+                            underline="hover"
+                            color="inherit"
+                            className='link transition !text-[14px]'
+                        >
+                            Men Opaque Casual Shirt
+                        </Link>
+                    </Breadcrumbs>
                 </div>
             </div>
-        </section>
+
+            <section className='bg-white py-5'>
+                <div className='container flex gap-8'>
+                    <div className='productZoomContainer w-[40%]'>
+                        <ProductZoom />
+                    </div>
+
+                    <div className='productContent w-[60%]'>
+                        <h1 className='text-[22px] font-[600] mb-2'>Men Opaque Casual Shirt</h1>
+                        <div className='flex items-center gap-3'>
+                            <span className='text-gray-400 text-[13px]'>
+                                Brands:
+                                <span className='font-[500] text-black opacity-75'>CLAFOUTIS</span>
+                            </span>
+
+                            <Rating name="size-small" defaultValue={2} size="small" readOnly />
+                            <span className='text-[13px] cursor-pointer'>Review (5)</span>
+                        </div>
+                        <div className='flex items-center gap-4 mt-4'>
+                            <span className='oldPrice line-through text-gray-500 text-[18px] font-[500]'>$58.00</span>
+                            <span className='price text-primary text-[18px] font-[600]'>$50.00</span>
+                        </div>
+                        <br />
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 export default ProductDetails;
--- src/components/ProductZoom/index.jsx
+++ src/components/ProductZoom/index.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import InnerImageZoom from 'react-inner-image-zoom';
+import 'react-inner-image-zoom/lib/InnerImageZoom/styles.css';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Navigation } from 'swiper/modules';
 
 const ProductZoom = () => {
 
+    const [slideIndex, setSlideIndex] = useState(0);
+    const zoomSliderBig = useRef();
+    const zoomSliderSml = useRef();
+    const goto = (index) => {
+        setSlideIndex(index);
+        zoomSliderSml.current.swiper.slideTo(index);
+        zoomSliderBig.current.swiper.slideTo(index);
+    }
+
     return (
         <>
+            <div className='flex gap-3'>
+                <div className='slider w-[15%]'>
+                    <Swiper
+                        ref={zoomSliderSml}
+                        direction={'vertical'}
+                        slidesPerView={5}
+                        spaceBetween={0}
+                        navigation={true}
+                        modules={[Navigation]}
+                        className="zoomProductSliderThumbs h-[500px] overflow-hidden"
+                    >
+                        <SwiperSlide>
+                            <div className={`item rounded-md overflow-hidden cursor-pointer group ${slideIndex === 0 ? 'opacity-1' : 'opacity-30'}`} onClick={() => goto(0)}>
+                                <img src="https://serviceapi.spicezgold.com/download/1742463096955_hbhb1.jpg" className='w-full transition-all group-hover:scale-105' />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={`item rounded-md overflow-hidden cursor-pointer group ${slideIndex === 1 ? 'opacity-1' : 'opacity-30'}`} onClick={() => goto(1)}>
+                                <img src="https://serviceapi.spicezgold.com/download/1742463096956_hbhb2.jpg" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={`item rounded-md overflow-hidden cursor-pointer group ${slideIndex === 2 ? 'opacity-1' : 'opacity-30'}`} onClick={() => goto(2)}>
+                                <img src="https://serviceapi.spicezgold.com/download/1742463096960_hbhb3.jpg" />
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className={`item rounded-md overflow-hidden cursor-pointer group ${slideIndex === 3 ? 'opacity-1' : 'opacity-30'}`} onClick={() => goto(3)}>
+                                <img src="https://serviceapi.spicezgold.com/download/1742463096961_hbhb4.jpg" />
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
+                <div className='zoomContainer w-[85%] h-[500px] overflow-hidden'>
+                    <Swiper
+                        ref={zoomSliderBig}
+                        slidesPerView={1}
+                        spaceBetween={0}
+                        navigation={false}
+                    >
+                        <SwiperSlide>
+                            <InnerImageZoom zoomType='hover' zoomScale={1} src='https://serviceapi.spicezgold.com/download/1742463096955_hbhb1.jpg' />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <InnerImageZoom zoomType='hover' zoomScale={1} src='https://serviceapi.spicezgold.com/download/1742463096956_hbhb2.jpg' />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <InnerImageZoom zoomType='hover' zoomScale={1} src='https://serviceapi.spicezgold.com/download/1742463096960_hbhb3.jpg' />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <InnerImageZoom zoomType='hover' zoomScale={1} src='https://serviceapi.spicezgold.com/download/1742463096961_hbhb4.jpg' />
+                        </SwiperSlide>
+                    </Swiper>
+
+                </div>
+            </div>
         </>
     )
 }
